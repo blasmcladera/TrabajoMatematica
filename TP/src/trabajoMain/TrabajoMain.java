713d66b80b827e3a6e0e63b812e81ef6dc9752aa
blasmcladera <blascladera715@gmail.com>
2024-05-04 13:37:25 -0300

    Agrego: canales ROJO, VERDE y AZUL, clear.

--- TP/src/trabajoMain/TrabajoMain.java
+++ TP/src/trabajoMain/TrabajoMain.java
@@ -18,11 +18,13 @@ public class TrabajoMain {
 
     public static void main(String[] args) {
         try {
+        	clearScreen();
             Scanner in = new Scanner(System.in);
             //Desde la consola ingreso ruta del archivo.
             System.out.println("Ingrese ruta de la imagen: ");
             String stringImagen = in.nextLine();
-            File imagen = new File(stringImagen);
+            String output = (stringImagen.replaceAll("'", "")).replaceAll(" ", "");
+            File imagen = new File(output);
             BufferedImage image = ImageIO.read(imagen);
 
             // Obtener la resolución del monitor del sistema
@@ -47,9 +49,23 @@ public class TrabajoMain {
             System.out.println("Presione ENTER para mostrar imagen RGB (cerrar imagen con tecla ENTER): ");
             stringImagen = in.nextLine();
             mostrarMatrizImagenRGB(matrizRGB);
-            System.out.println("Presione ENTER para mostrar imagen en escala de grises (cerrar imagen con tecla ENTER): ");
+            clearScreen();
+            System.out.println("Presione ENTER para mostrar imagen en escala de grises canal ROJO (cerrar imagen con tecla ENTER): ");
+            stringImagen = in.nextLine();
+            mostrarMatrizImagenRojo(matrizRGB);
+            clearScreen();
+            System.out.println("Presione ENTER para mostrar imagen en escala de grises canal VERDE (cerrar imagen con tecla ENTER): ");
+            stringImagen = in.nextLine();
+            mostrarMatrizImagenVerde(matrizRGB);
+            clearScreen();
+            System.out.println("Presione ENTER para mostrar imagen en escala de grises canal AZUL (cerrar imagen con tecla ENTER): ");
+            stringImagen = in.nextLine();
+            mostrarMatrizImagenAzul(matrizRGB);
+            clearScreen();
+            System.out.println("Presione ENTER para mostrar imagen en escala de grises PROMEDIO (cerrar imagen con tecla ENTER): ");
             stringImagen = in.nextLine();
             int[][] matrizGrises = escalaDeGrises(matrizRGB);
+            clearScreen();
             System.out.println("Programa finalizado.");
             in.close();
 
@@ -82,7 +98,7 @@ public class TrabajoMain {
         BufferedImage imagenNueva = crearImagenMatrizGris(matrizGris);
         ImageIcon icon = new ImageIcon(imagenNueva);
         JLabel label = new JLabel(icon);
-        JFrame frame = new JFrame("MatrizGris:");
+        JFrame frame = new JFrame("Matriz GRISES:");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(label);
 
@@ -131,7 +147,7 @@ public class TrabajoMain {
         BufferedImage imagenNueva = crearImagenMatrizRGB(matrizRGB);
         ImageIcon icon = new ImageIcon(imagenNueva);
         JLabel label = new JLabel(icon);
-        JFrame frame = new JFrame("MatrizRGB:");
+        JFrame frame = new JFrame("Matriz RGB:");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(label);
 
@@ -174,6 +190,160 @@ public class TrabajoMain {
         }
 
         return imagen;
+    }
+    
+    private static void mostrarMatrizImagenRojo(int[][][] matrizRGB) {
+        BufferedImage imagenNueva = crearImagenMatrizRojo(matrizRGB);
+        ImageIcon icon = new ImageIcon(imagenNueva);
+        JLabel label = new JLabel(icon);
+        JFrame frame = new JFrame("Matriz canal ROJO:");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(label);
+
+        // Establecer el tamaño de la ventana como el tamaño de la imagen
+        frame.pack();
+
+        frame.setVisible(true);
+
+        final JFrame finalFrame = frame;
+
+        frame.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    finalFrame.dispose();
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+    }
+
+    private static BufferedImage crearImagenMatrizRojo(int[][][] matrizRGB) {
+        int altura = matrizRGB.length;
+        int ancho = matrizRGB[0].length;
+        BufferedImage imagen = new BufferedImage(ancho, altura, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < ancho; x++) {
+                int red = matrizRGB[y][x][0];
+                Color color = new Color(red, red, red);
+                imagen.setRGB(x, y, color.getRGB());
+            }
+        }
+
+        return imagen;
+    }
+    
+    private static void mostrarMatrizImagenVerde(int[][][] matrizRGB) {
+        BufferedImage imagenNueva = crearImagenMatrizVerde(matrizRGB);
+        ImageIcon icon = new ImageIcon(imagenNueva);
+        JLabel label = new JLabel(icon);
+        JFrame frame = new JFrame("Matriz canal VERDE:");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(label);
+
+        // Establecer el tamaño de la ventana como el tamaño de la imagen
+        frame.pack();
+
+        frame.setVisible(true);
+
+        final JFrame finalFrame = frame;
+
+        frame.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    finalFrame.dispose();
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+    }
+
+    private static BufferedImage crearImagenMatrizVerde(int[][][] matrizRGB) {
+        int altura = matrizRGB.length;
+        int ancho = matrizRGB[0].length;
+        BufferedImage imagen = new BufferedImage(ancho, altura, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < ancho; x++) {
+                int green = matrizRGB[y][x][1];
+                Color color = new Color(green, green, green);
+                imagen.setRGB(x, y, color.getRGB());
+            }
+        }
+
+        return imagen;
+    }
+    
+    private static void mostrarMatrizImagenAzul(int[][][] matrizRGB) {
+        BufferedImage imagenNueva = crearImagenMatrizAzul(matrizRGB);
+        ImageIcon icon = new ImageIcon(imagenNueva);
+        JLabel label = new JLabel(icon);
+        JFrame frame = new JFrame("Matriz canal AZUL:");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(label);
+
+        // Establecer el tamaño de la ventana como el tamaño de la imagen
+        frame.pack();
+
+        frame.setVisible(true);
+
+        final JFrame finalFrame = frame;
+
+        frame.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+            }
+
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    finalFrame.dispose();
+                }
+            }
+
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+    }
+
+    private static BufferedImage crearImagenMatrizAzul(int[][][] matrizRGB) {
+        int altura = matrizRGB.length;
+        int ancho = matrizRGB[0].length;
+        BufferedImage imagen = new BufferedImage(ancho, altura, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < ancho; x++) {
+                int blue = matrizRGB[y][x][2];
+                Color color = new Color(blue, blue, blue);
+                imagen.setRGB(x, y, color.getRGB());
+            }
+        }
+
+        return imagen;
+    }
+    
+    public static void clearScreen() {
+        // Limpia la consola utilizando comandos específicos para sistemas operativos
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException e) {
+            // Maneja cualquier excepción que pueda ocurrir al limpiar la consola
+            e.printStackTrace();
+        }
     }
 
 }
