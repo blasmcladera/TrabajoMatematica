79072155b06ef1a1f589a6b5c62a44b0a7b758ec
blasmcladera <blascladera715@gmail.com>
2024-05-09 22:45:38 -0300

    Hacemos tp

--- TP/src/trabajoMain/Tp2.java
+++ TP/src/trabajoMain/Tp2.java
@@ -9,6 +9,24 @@ import java.util.Scanner;
 
 public class Tp2 {
 	
+	public static final double[][] Emboss = {
+	        {-2, -1, 0},
+	        {-1, 1, 1},
+	        {0, 1, 2}
+	    };
+	
+	public static final double[][] Sobel = {
+	        {-1, 0, 1},
+	        {-2, 0, 2},
+	        {-1, 0, 1}
+	    };
+	
+	public static final double[][] Sharpen = {
+			{-1, -1,-1},
+	        {-1, 9,-1},
+	        {-1, -1,-1}
+	    };
+	
 
 	public static void main(String[] args) {
 		try {
@@ -24,7 +42,7 @@ public class Tp2 {
 		int [][] matrizRojos =  new int[alto][ancho];
 		int [][] matrizVerdes = new int[alto][ancho];
 		int [][] matrizAzules = new int[alto][ancho];
-		int [][] matrizGrises = new int[alto][ancho];
+		double [][] matrizGrises = new double[alto][ancho];
 		
 		for (int i=0;i<alto;i++) {
 			for (int j=0;j<ancho; j++) {
@@ -49,6 +67,19 @@ public class Tp2 {
 		File imagenGrises = new File("imagenGrises.jpg");
 		ImageIO.write(imagenNueva, "jpg", imagenGrises);
 		
+		double [][] matrizFiltro = convolucion(matrizGrises,Sobel,alto,ancho);
+		
+		BufferedImage matrizFiltrada = new BufferedImage(ancho-1,alto-1,BufferedImage.TYPE_INT_RGB);
+		
+		for (int i=0;i<alto-1;i++) {
+			for (int j =0; j<ancho-1; j++) {
+				Color gris = new Color((int)matrizFiltro[i][j],(int)matrizFiltro[i][j],(int)matrizFiltro[i][j]);
+				matrizFiltrada.setRGB(j, i,gris.getRGB());
+			}
+		}
+		
+		File imagenFiltrada = new File("imagenFiltrada.jpg");
+		ImageIO.write(matrizFiltrada, "jpg", imagenFiltrada);
 		
 		
 		in.close();
@@ -56,6 +87,30 @@ public class Tp2 {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static double [][] convolucion (double [][] matrizGrises,double [][] kernel, int alto, int ancho) {
+		double aux;
+		double [][] matrizFiltrada = new double[alto-1][ancho-1];
+		int altk,anck;
+		for (int i=1;i<alto-1;i++) {
+			for (int j=1;j<ancho-1; j++) {
+				aux=0;
+				altk=0;
+				for (int k=i-1;k<i+2;k++) {
+					anck=0;
+					for (int h=j-1; h<j+2;h++) {
+					aux= (aux + (matrizGrises[k][h]*kernel[altk][anck]));
+					anck++;
+					}
+					altk++;
+				}
+				if (aux<0) aux=0;
+				if (aux>255) aux=255;
+				matrizFiltrada[i][j] = aux;
+			}
+		}
+		return matrizFiltrada;
 	}
 
 }
